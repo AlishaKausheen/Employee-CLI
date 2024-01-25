85bb3de261b6555d48be627df4d6c23c3e4710f4
AlishaKausheen <alishakausheen@gmail.com>
2024-01-25 07:46:35 -0800

    add employee command

---
+++ command.js
@@ -0,0 +1,19 @@
+const program = require("commander");
+const {
+    addEmployee,
+    findEmployee
+} = require("./index");
+
+program
+    .version('1.0.0')
+    .description('Employee Management System')
+
+program
+    .command(`add <firstName> <lastName> <phone> <email>`)
+    .alias('a')
+    .description('Add an employee')
+    .action((firstName, lastName, phone, email) => {
+        addEmployee({ firstName, lastName, phone, email });
+    })
+
+program.parse(program.argv);
